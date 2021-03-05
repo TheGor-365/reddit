9be9f215014f5a39da14d2476355719b633485f6
Gor <7495500@gmail.com>
2021-03-05 23:23:36 +0300

    Add actions for public and communities controllers

--- app/controllers/communities_controller.rb
+++ app/controllers/communities_controller.rb
@@ -1,21 +1,39 @@
 class CommunitiesController < ApplicationController
 
   before_action :authenticate_account!, expect: [ :index, :show ]
+  before_action :set_community, only: [ :show ]
 
   def index
     @communities = Community.all
   end
 
   def new
-
+    @community = Community.new
   end
 
   def create
+    @community = Community.new(community_params)
+    @community.account_id = current_account.id
 
+    if @community.save
+      redirect_to communities_path
+    else
+      render :new
+    end
   end
 
   def show
 
+  end
+
+  private
+
+  def set_community
+    @community = Community.find(params[:id])
+  end
+
+  def community_params
+    params.required(:community).permit(:name, :url, :rules)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
 
   devise_for :accounts
   root to: "public#index"
-  
+
+  resources :communities
+
 end
